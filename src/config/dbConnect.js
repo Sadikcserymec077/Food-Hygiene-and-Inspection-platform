996e7d40d453eb0e80725de8479fac07142ae769
KMohammedsadiq <mohammedsadiq4850@gmail.com>
2026-04-25 11:57:48 +0530

    Add SSL and port support for Aiven cloud MySQL

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -4,12 +4,14 @@ dotenv.config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
 });
 
 (async () => {
